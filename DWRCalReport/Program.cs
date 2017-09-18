d102188cffc4f229101fa5b8e9369fd97d3b8e9b
JD Marymee <jd@tech-innovators.com>
2017-09-18 07:50:53 -0700

    Added for UI and output code

--- DWRCalReport/Program.cs
+++ DWRCalReport/Program.cs
@@ -16,8 +16,20 @@ namespace DWRCalReport
             Console.ReadLine();
         }
 
+        public void WriteDWRItem(DWRItemClass item)
+        {
+            Console.WriteLine(String.Format("{0}", item.Categories));
+            Console.WriteLine(String.Format("{0}", item.Subject));
+            Console.WriteLine(String.Format("{0}", item.startDate));
+            Console.WriteLine(String.Format("{0}", item.endDate));
+            Console.WriteLine(String.Format("{0}", item.duration));
+            Console.WriteLine(String.Format("{0}", item.isAllDay));
+        }
+
         public void GetAllCalendarItems()
         {
+            List<DWRItemClass> dwrItems = new List<DWRItemClass>();
+
             Microsoft.Office.Interop.Outlook.Application oApp = null;
             Microsoft.Office.Interop.Outlook.NameSpace mapiNamespace = null;
             Microsoft.Office.Interop.Outlook.MAPIFolder CalendarFolder = null;
@@ -25,7 +37,8 @@ namespace DWRCalReport
 
             oApp = new Microsoft.Office.Interop.Outlook.Application();
             mapiNamespace = oApp.GetNamespace("MAPI"); ;
-            CalendarFolder = mapiNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderCalendar); outlookCalendarItems = CalendarFolder.Items;
+            CalendarFolder = mapiNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderCalendar);
+            outlookCalendarItems = CalendarFolder.Items;
             outlookCalendarItems.IncludeRecurrences = true;
 
             foreach (Microsoft.Office.Interop.Outlook.AppointmentItem item in outlookCalendarItems)
@@ -36,8 +49,6 @@ namespace DWRCalReport
                     DateTime first = new DateTime(2017, 9, 17, item.Start.Hour, item.Start.Minute, 0);
                     DateTime last = new DateTime(2017, 9, 17);
                     Microsoft.Office.Interop.Outlook.AppointmentItem recur = null;
-
-
 
                     for (DateTime cur = first; cur <= last; cur = cur.AddDays(1))
                     {
@@ -57,19 +68,24 @@ namespace DWRCalReport
                     {
                         //MessageBox.Show(item.Subject + " -> " + item.Start.ToLongDateString());
                         //Console.WriteLine(item.Subject + " -> " + item.Start.ToLongDateString());
-                        if (item.Categories.Contains("DWR"))
+                        if (item.Categories.Contains("DWR:"))
                         {
-                            Console.WriteLine(String.Format("{0}", item.Categories));
-                            Console.WriteLine(String.Format("{0}", item.Subject));
-                            Console.WriteLine(String.Format("{0}", item.Start));
-                            Console.WriteLine(String.Format("{0}", item.End));
-                            Console.WriteLine(String.Format("{0}", item.Duration));
-                            Console.WriteLine(String.Format("{0}", item.AllDayEvent));
+                            dwrItems.Add(new DWRItemClass() { Categories = item.Categories, duration = item.Duration, endDate = item.End, isAllDay = item.AllDayEvent, startDate = item.Start, Subject = item.Subject });
                         }
                     }
                 }
             }
 
         }
+    }
+
+    public class DWRItemClass
+    {
+        public string Categories { get; set; }
+        public string Subject { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int duration { get; set; }
+        public bool isAllDay { get; set; }
     }
 }
---
+++ DWRReport/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DWRReport
+{
+    public partial class DWRForm1 : Form
+    {
+        DataTable dt;
+        DataRow dr;
+        DataColumn idColumn;
+        DataColumn nameColumn;
+        BindingSource bindingSource1;
+        public DWRForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet("DWR");
+            bindingSource1 = new BindingSource();
+
+            dt = new DataTable();
+            idColumn = new DataColumn("ID", Type.GetType("System.Int32"));
+            nameColumn = new DataColumn("Name", Type.GetType("System.String"));
+
+            dt.Columns.Add(idColumn);
+            dt.Columns.Add(nameColumn);
+
+            dr = dt.NewRow();
+            dr["ID"] = 1;
+            dr["Name"] = "Name1";
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+
+            bindingSource1.DataSource = ds;
+
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+            dataGridView1.DataSource = bindingSource1;
+        }
+    }
+}
